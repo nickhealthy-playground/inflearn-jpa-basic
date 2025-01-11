185e2002d0da1a84a586592d99860612e80cc03b
nickhealthy <alskadmlcraz1@gmail.com>
2025-01-11 18:03:46 +0900

    4. 엔티티 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,8 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 // JPA가 관리할 객체
 @Entity
@@ -14,6 +14,19 @@ public class Member {
     // DDL 생성 기능
     @Column(unique=true, length = 10)
     private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
 
     public Member() {
     }
