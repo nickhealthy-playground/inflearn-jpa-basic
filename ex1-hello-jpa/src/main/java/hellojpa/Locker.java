b784e5fcc632740cb60a8b6b283ed225562cb673
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-07 16:23:17 +0900

    7.다양한 연관관계 매핑 - 일대일[1:1]

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Locker.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String username;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+
+}
