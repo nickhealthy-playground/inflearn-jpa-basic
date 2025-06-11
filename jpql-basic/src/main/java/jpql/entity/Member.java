5a4b68ae8959c57b38cb0bacf905f0f2c657b216
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-11 14:37:46 +0900

    11. 객체지향쿼리 기본문법 - 기본 문법과 쿼리 API

---
+++ jpql-basic/src/main/java/jpql/entity/Member.java
@@ -0,0 +1,55 @@
+package jpql.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "member_seq")
+    @SequenceGenerator(name = "member_seq",
+            sequenceName = "MEMBER_SEQ",
+            allocationSize = 1)
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ jpql-basic/src/main/java/jpql/entity/Order.java
@@ -0,0 +1,54 @@
+package jpql.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int orderAmount;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    @Embedded
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(int orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
